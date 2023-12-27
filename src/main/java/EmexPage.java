2498384ec0aa4627ca6a637896b2917550b7abd8
“Dzmitry <slask@tut.by>
2023-12-27 22:22:37 +0300

    rebase Ktalogtest

--- src/main/java/EmexPage.java
+++ src/main/java/EmexPage.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmexPage {
@@ -10,8 +11,12 @@ public class EmexPage {
     private String firstShop = "//*[@data-testid='MapSearch:text:sideBarItemTitle[0]']";
     private String buttonAddBasket = "//div[@class='sc-6610f28-18 jZDDSd']//span";
     private String buttonBasket = "//div[@class='sc-2f223a1-2 BBpCm']";
-    private String textBasketSparePart ="//a[@href='/products/1051859/Ford/17207']";
-    private String itemsCatalog ="//div[@class=\"sc-480f8902-2 kkFiSh\"]/div";
+    private String textBasketSparePart = "//a[@href='/products/1051859/Ford/17207']";
+    private String itemsCatalog = "//div[@class=\"sc-480f8902-2 kkFiSh\"]/div";
+    private String buttonSubmitAccount = "//a[@data-testid='Header:button:loginButton']";
+    private String buttonEnter = "//div[@class= 'sc-dac0c06d-1 flqRoP']";
+    private String inputLogin = "//input[@placeholder= 'Телефон, почта или id']";
+    private String inputPassword = "//input[@id='signInPasswordInput']";
 
     private WebDriver driver;
 
@@ -39,11 +44,48 @@ public class EmexPage {
         Thread.sleep(3000);
     }
 
-    public String getTextBasketSparePart(){
-        return  driver.findElement(By.xpath(textBasketSparePart)).getText();
+    public String getTextBasketSparePart() {
+        return driver.findElement(By.xpath(textBasketSparePart)).getText();
     }
 
     public List<WebElement> setItemCatalog() {
         return driver.findElements(By.xpath(itemsCatalog));
+    }
+
+    public void clickbuttonSubmitAccount() throws InterruptedException {
+        driver.findElement(By.xpath(buttonSubmitAccount)).click();
+        Thread.sleep(3000);
+    }
+
+    public void setInputLogin() {
+        driver.findElement(By.xpath(inputLogin)).sendKeys("+79878342494");
+    }
+
+    public void clickButtonEnter() {
+        driver.findElement(By.xpath(buttonEnter)).click();
+    }
+
+    public String getPasswordsBordColor() {
+        return driver.findElement(By.xpath(inputPassword)).getCssValue("border-color");
+    }
+
+    public List<String> addElementsKatalogItems() {
+        List<WebElement> itemList = setItemCatalog();
+        List<String> catalogItemList = new ArrayList<>();
+        for (WebElement itemlist : itemList) {
+            catalogItemList.add(itemlist.getText());
+            System.out.println(itemlist.getText());
+        }
+        return catalogItemList;
+    }
+
+    public List<String> addExpectedKatalogItems() {
+        List<String> expectedKatalogItems = new ArrayList<>();
+        expectedKatalogItems.add("Часто покупают");
+        expectedKatalogItems.add("Запчасти из-за границы");
+        expectedKatalogItems.add("Автотовары");
+        expectedKatalogItems.add("Инструменты для СТО");
+        expectedKatalogItems.add("Для грузового транспорта");
+        return expectedKatalogItems;
     }
 }
--- src/test/java/EmexTest.java
+++ src/test/java/EmexTest.java
@@ -1,11 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class EmexTest  extends BasePage {
+public class EmexTest extends BasePage {
     @Test
     public void checkSpearPartInBasket() throws InterruptedException {
         EmexPage emexPage = new EmexPage(driver);
@@ -21,18 +17,16 @@ public class EmexTest  extends BasePage {
     public void testMenu() {
         EmexPage emexPage = new EmexPage(driver);
         driver.navigate().to("https://emex.ru/");
-        List<WebElement> itemList = emexPage.setItemCatalog();
-        List<String> catalogItemList = new ArrayList<>();
-        for (int i = 0; i < itemList.size(); i++) {
-            System.out.println(itemList.get(i).getText());
-            catalogItemList.add(itemList.get(i).getText());
-        }
-        List<String> expectedKatalogItems = new ArrayList<>();
-        expectedKatalogItems.add("Часто покупают");
-        expectedKatalogItems.add("Запчасти из-за границы");
-        expectedKatalogItems.add("Автотовары");
-        expectedKatalogItems.add("Инструменты для СТО");
-        expectedKatalogItems.add("Для грузового транспорта");
-        Assertions.assertEquals(expectedKatalogItems, catalogItemList);
+        Assertions.assertEquals(emexPage.addExpectedKatalogItems(), emexPage.addElementsKatalogItems());
+    }
+
+    @Test
+    public void checkEnterWithEmptyPassword() throws InterruptedException {
+        EmexPage emexPage = new EmexPage(driver);
+        driver.navigate().to("https://emex.ru/");
+        emexPage.clickbuttonSubmitAccount();
+        emexPage.setInputLogin();
+        emexPage.clickButtonEnter();
+        Assertions.assertEquals("rgb(255, 0, 0)", emexPage.getPasswordsBordColor());
     }
 }
